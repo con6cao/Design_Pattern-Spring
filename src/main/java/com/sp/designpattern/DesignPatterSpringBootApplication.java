61268abc3243866aa1362ec1a0e5f7a2d71ddefa
CVdaZN <caio_vinicius2246@hotmail.com>
2024-08-18 03:15:18 -0300

    Definição da camada Model e suas classes

---
+++ src/main/java/com/sp/designpattern/DesignPatterSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sp.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatterSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatterSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sp/designpattern/model/Cliente.java
@@ -0,0 +1,49 @@
+package com.sp.designpattern.model;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "Cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, Endereco endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
